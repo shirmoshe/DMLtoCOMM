3a914d7a55633f438c94099334a2e54a37fb992e
shirmoshe <80037069+shirmoshe@users.noreply.github.com>
2025-04-27 13:04:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ import result_visualization
 def main():
 
     # ============================ INITIALIZATION ============================ #
-    model_path = "imagenet/resnet50-v1-7.onnx"  # replace with model path
+    #model_path = "imagenet/resnet50-v1-7.onnx"  # replace with model path
+    model_path = r"C:\Users\shirm\PycharmProjects\Project\load_model\tiny_llama_model\tiny_llama.onnx"  # replace with model path
     onnx_model = onnx_analyze.load_model(model_path)  # load the ONNX model
 
     os.makedirs("svg_file", exist_ok=True)     # make folder for the svg file
 
-    json_path = "user_inputs.json"      # load user inputs json file
+    json_path = r"C:\Users\shirm\PycharmProjects\Project\load_model\code\user_inputs.json"      # load user inputs json file
     with open(json_path, 'r') as f:
         config = json.load(f)
 
@@ -41,17 +42,22 @@ def main():
     topology = Topology(topology_type, total_gpu)
     topology.add_GPU(d, t, p)
 
-    # Add coordinate to GPU
-
-
     # Create Node objects and build the hierarchy
     nodes_list = onnx_analyze.create_nodes(onnx_model)
+    layers = onnx_analyze.group_layer(nodes_list)
 
     # ============================ DATA PARALLELISM ============================ #
     model_replicas = data_parllel.create_data_parallel_collectives(nodes_list, d, data_size)  # replica model d times
     result_visualization.create_interactive_high_level_svg(model_replicas)      # create interactive high level graph
     for i, replica in enumerate(model_replicas):     # generate each detailed replica view
-        result_visualization.create_svg_graph(replica, output_file=f"gpu_{i}_detail")
+        result_visualization.create_svg_graph(replica, output_file=f"data_{i}_detail")
+
+    # ============================ PIPELINE PARALLELISM ============================ #
+
+    for d_idx in range(d):
+        result_visualization.create_layered_svg(layers, d_idx, "svg_layer_file")
+
+
 
 
 if __name__ == "__main__":
