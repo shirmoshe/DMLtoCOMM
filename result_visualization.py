e832d77f021255d14b3786a4d5f62e116e359c42
shirmoshe <80037069+shirmoshe@users.noreply.github.com>
2025-04-27 13:06:21 +0300

    Update result_visualization.py

--- result_visualization.py
+++ result_visualization.py
@@ -6,7 +6,7 @@ from graphviz import Digraph
 import webbrowser
 from collections import defaultdict
 import data_parllel
-
+import os
 
 def create_svg_graph(nodes_list, output_file="onnx_model_graph"):
     """Generate an SVG graph of the nodes using Graphviz and open it automatically."""
@@ -17,7 +17,7 @@ def create_svg_graph(nodes_list, output_file="onnx_model_graph"):
     for node in nodes_list:
         # Build label: name, op_type
 #        gpu_num = f"data id {node.id_d}" if node.id_d is not None else "No GPU"
-        label = f"data id: {node.id_d}\n{node.name}\n({node.op_type})\ndata_size: {node.data_size}"
+        label = f"data id: {node.id_d}\nlayer: {node.layer}\n{node.name}\n({node.op_type})\ndata_size: {node.data_size}"
         dot.node(str(id(node)), label=label)
 
     # Add edges
@@ -33,7 +33,7 @@ def create_svg_graph(nodes_list, output_file="onnx_model_graph"):
     #webbrowser.open_new_tab(out_path)
 
 
-# unused function
+
 def create_svg_graph_with_clusters(nodes_list, output_file="clustered_graph"):
     """
     Generate an SVG graph using Graphviz where each GPU's nodes are grouped into a subgraph (cluster).
@@ -56,7 +56,7 @@ def create_svg_graph_with_clusters(nodes_list, output_file="clustered_graph"):
             label = f"{node.name}\n({node.op_type}\ndata id: {node.id_d})"
             dot.node(str(id(node)), label=label, shape="box", style="filled", fillcolor="lightblue")
 
-    # Add clusters per GPU
+    # Add clusters per data
     for gpu, group_nodes in gpu_groups.items():
         with dot.subgraph(name=f"cluster_gpu_{gpu}") as c:
             c.attr(label=f"GPU {gpu}")
@@ -107,7 +107,7 @@ def create_interactive_high_level_svg(model_replicas, output_file="interactive_h
     for gpu, group_nodes in gpu_groups.items():
         gpu_node_name = f"gpu_{gpu}"
         label = f"Data id: {gpu}\n(ReplicaModel)"
-        href = f"gpu_{gpu}_detail.svg"
+        href = f"data_{gpu}_detail.svg"
 
         dot.node(gpu_node_name, label=label, shape="box3d", style="filled", fillcolor="lightgray", href=href, target="_blank")
 
@@ -123,3 +123,56 @@ def create_interactive_high_level_svg(model_replicas, output_file="interactive_h
     out_path = dot.render(f"svg_file/{output_file}", view=False)
     print(f"Interactive high-level SVG saved to: {out_path}")
     webbrowser.open_new_tab(out_path)
+
+
+def create_layered_svg(layer_groups, d_id=0, output_dir="svg_layer_file"):
+    """
+    create high level interactive graph with layers
+    """
+    # make sure the folder exist, if not - open a new one
+    os.makedirs(output_dir, exist_ok=True)
+
+    dot = Digraph(comment=f"Layered Graph for data replica {d_id}", format='svg')
+    dot.attr(rankdir='TB', compound='true')
+
+    for layer_idx, nodes in layer_groups.items():
+        box_id = f"layer_{d_id}_{layer_idx}"
+        label = f"layer {layer_idx}"
+        href = f"layer_{layer_idx}_data_{d_id}_detail.svg"
+        dot.node(box_id, label=label, shape="box", style="filled", fillcolor="lightyellow", href=href, target="_blank")
+
+        # create subgraph for each layer
+        save_layer_detail_svg(nodes, layer_idx, d_id, output_dir)
+
+    # add global edges between layers
+    for target_layer, nodes in layer_groups.items():
+        for node in nodes:
+            for parent in node.parents:
+                source_layer = getattr(parent, 'layer', -1)
+                if source_layer != target_layer:
+                    src = f"layer_{d_id}_{source_layer}"
+                    tgt = f"layer_{d_id}_{target_layer}"
+                    dot.edge(src, tgt)
+
+    out_path = dot.render(filename=f"{output_dir}/data_{d_id}_detail", view=False)
+    print(f"Graph saved as: {output_dir}/data_{d_id}_detail")
+
+
+def save_layer_detail_svg(layer_nodes, layer_idx, d_id, output_dir="svg_file"):
+    """
+    save layer's subgraph
+    """
+    dot = Digraph(comment=f"Layer {layer_idx} data {d_id} Detail", format="svg")
+
+    for node in layer_nodes:
+        label = f"{node.name}\n({node.op_type}\nlayer: {node.layer})"
+        dot.node(str(id(node)), label=label)
+
+    for node in layer_nodes:
+        for parent in node.parents:
+            if parent in layer_nodes:
+                dot.edge(str(id(parent)), str(id(node)))
+
+    filename = f"{output_dir}/layer_{layer_idx}_data_{d_id}_detail"
+    dot.render(filename=filename, view=False)
+    print(f"Graph saved as: {filename}.svg")
